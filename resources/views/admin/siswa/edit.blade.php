450ebbec05b7ad8997a529484454cc8d37b2517c
satoshisensei <satriai418@gmail.com>
2022-01-13 17:57:08 -0800

    Create & Update 2

--- resources/views/admin/siswa/edit.blade.php
+++ resources/views/admin/siswa/edit.blade.php
@@ -1,5 +1,57 @@
 @extends('admin.layouts.app')
 
 @section('container')
-    
+    <div class="container container-fluid mb-3 d-flex justify-content-center">
+        <h2 class="h3 text-uppercase">Edit Data</h2>
+    </div>
+    <div class="container container-fluid d-flex justify-content-center">
+        <div class="col-md-8 ">
+            <div class="portlet light bordered">
+                    <div class="form-body">
+                        <form action="/siswa/{{ $siswas[0]->id }}" method="post">
+                        @method('put')
+                        @csrf
+                        <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+                        <div class="mb-3">
+                            <label for="nama" class="form-label">Nama</label>
+                            <input type="text" class="form-control @error('nama')
+                                is-invalid
+                            @enderror" id="nama" name="nama" placeholder="Masukan Nama..." autofocus required value="{{ old('nama',$siswas[0]->nama) }}">
+                            @error('nama')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="nip" class="form-label">Nip</label>
+                            <input type="text" class="form-control @error('nis')
+                                is-invalid
+                            @enderror" id="nip" name="nip" placeholder="Masukan Nis..." required value="{{ old('nis',$siswas[0]->nis) }}">
+                            @error('nip')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="alamat" class="form-label">Alamat</label>
+                            <input id="alamat" type="hidden" name="alamat" value="{{ old('alamat',$siswas[0]->alamat) }}">
+                            <trix-editor input="alamat"></trix-editor>
+                            @error('alamat')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <a href="{{ url()->previous() }}" class="btn btn-primary me-md-2">Kembali</a>
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
